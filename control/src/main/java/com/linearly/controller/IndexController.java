ea355e82c0a095a19a2712620bcf4fb0f9151b30
EvanYohe <Yohe.evanJ@gmail.com>
2024-11-12 09:04:13 -0800

    abba

--- control/src/main/java/com/linearly/controller/IndexController.java
+++ control/src/main/java/com/linearly/controller/IndexController.java
@@ -1,40 +1,55 @@
 package com.linearly.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
-
-import com.linearly.model.Dimension;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-@RequestMapping("/")
 public class IndexController {
 
-    @GetMapping("/") // removing this mapping wil break the index page
-    public String index() {
-
-        return "page";
+    /*
+     * Maps the HTTP GET request to the root URL to the index.jte file.
+     * 
+     * @param Model the model object to be used in the view
+     * @return String: the name of the template file to render in the view
+     */
+    @GetMapping("/")
+    public String index(Model Model) {
+        return "index";
     }
 
-    @PostMapping("matrixmodule")
-    public String matrixModule(@ModelAttribute Dimension dimension, Model model) {
-
-        model.addAttribute("dimension", dimension);
-        return "matrixmodule";
+    /*
+     * Maps the HTTP POST request to the matrix-module URL to the matrix-module.jte file.
+     * 
+     * @param Model the model object to be used in the view
+     * @return String: the name of the template file to render in the view
+     */
+    @PostMapping("/matrix-module")
+    public String loadMatrixModule(Model model) {
+        return "matrix-module";
     }
 
-    @PostMapping("eliminationmodule")
-    public String eliminationModule() {
-
-        return "eliminationmodule";
+    /*
+     * Maps the HTTP POST request to the elimination-module URL to the elimination-module.jte file.
+     * 
+     * @param Model the model object to be used in the view
+     * @return String: the name of the template file to render in the view
+     */
+    @PostMapping("/elimination-module")
+    public String loadEliminationModule(Model model) {
+        return "elimination-module";
     }
 
-    @PostMapping("vectorspacemodule")
-    public String vectorspaceModule() {
-
-        return "vectorspacemodule";
+    /* 
+     * TODO: replace temporary return value when vectorspace-module.jte is created
+     * Maps the HTTP POST request to the vectorspace-module URL to the vectorspace-module.jte file.
+     * 
+     * @param Model the model object to be used in the view
+     * @return String: the name of the template file to render in the view
+     */
+    @PostMapping("/vectorspace-module")
+    public String loadVectorspaceModule(Model model) {
+        return "matrix-module";
     }
 }
--- control/src/main/java/com/linearly/controller/VectorspaceController.java
+++ control/src/main/java/com/linearly/controller/VectorspaceController.java
@@ -1,5 +1,8 @@
 package com.linearly.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class VectorspaceController {
 
 }
